aca76e036dfa83d135d571ba94c6ef5ffb315af6
Hiromi Kageyama <hpkageyama@alaska.edu>
2023-10-16 16:43:29 -0800

    wiki created

--- forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class NewEntryForm(forms.Form):
-    title = forms.CharField(label="Title")
-    newentry = forms.CharField(widget=forms.Textarea, label="New Entry")
-    
